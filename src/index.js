bcf66ce9c129af7fe26f6bd369d6916bbe6c38d2
Mlkrbltzr <ge.astudillo.aray@gmail.com>
2023-09-14 20:49:30 -0300

    agregando router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import express from "express"
-import ProductManager from "./controllers/ProductManager.js";
-const product = new ProductManager ();
+import express from "express";
+import productRouter from "./router/product.routes.js";
+import CartRouter from "./router/cart.routes.js";
 
-const app = express ()
-const PORT = 4000 /*verificar con el puerto que piden en la prueba*/
+const app = express();
+const PORT = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
+app.use(express.urlencoded({extended:true}));
 
+app.use("/products", productRouter)
+app.use("/api/cart", CartRouter)
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Servidor express funcionando en el puerto ${PORT}`);
+})
 
